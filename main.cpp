79277ca0d61c5d05e4bc2d02d55eadb2a0e71bd6
AchPaudel <70857662+AchPaudel@users.noreply.github.com>
2020-09-09 18:59:54 -0500

    Updating empty file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
 
+#include <iostream>
+#include "Executive.h"			//game tasks
+
+int main()
+{
+	std::cout << "Welcome to BattleShip!\n";		
+	Executive exec;	//executive object for all game tasks
+	return 0;	
+}
