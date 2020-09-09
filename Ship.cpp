a0c65389aa093296f89916d073d6cf9373cf76e8
linhden2000 <66048229+linhden2000@users.noreply.github.com>
2020-09-09 11:15:51 -0500

    Add files via upload

---
+++ Ship.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Ship {
+    private: 
+        int size;
+        char orientation;
+
+        int startRow;
+        int endRow;
+
+        char startCol;
+        char endCol;
+
+        int *row;
+        int *col;
+        
+    public:
+        //Constructor
+        Ship(int size, char orientation, int startRow, int endRow, char startCol, char endCol, int *row, char *col);
+        ~Ship();
+        //return size of a ship
+        int getSize();
+
+        //return orientation either 'horizontal' and 'vertical'
+        char getOrientation();
+
+        //if player hit the ship, change the row to 0 and col to 'O'
+        void fireSpace(int rowPosition, char colPosition);
+
+        //Check the ship is sunk
+        bool isSunk(int size);
+        
+}
