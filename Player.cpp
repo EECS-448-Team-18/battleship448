3eb390725214618635dccd801c1e6ed2a8e6689f
Achyut Paudel <achpaudel@ku.edu>
2020-09-10 13:11:21 -0500

    added new functions and defined most of them.

--- Player.cpp
+++ Player.cpp
@@ -6,10 +6,30 @@ using namespace std;
 
 Player::Player(int numOfBoats){
   _numOfBoats = numOfBoats;
+  boardPtr = new Board();
 }
 
 
 int Player::getNumOfBoats(){
-  return _numOfBoats;
+  return(_numOfBoats);
 }
 
+void setGuess(string location) {
+    _guessShot = location;
+}
+
+string getGuess() {
+    return(_guessShot);
+}
+
+*Board getBoard() const {
+    return(*boardPtr);
+}
+
+bool gettingShot(string location) {
+    
+}
+
+void shooting(string location, bool hit) {
+
+}
--- Player.h
+++ Player.h
@@ -1,6 +1,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <string>
+#include "Board.h"
 using namespace std;
 
 class Player{
@@ -8,9 +9,16 @@ public:
   Player(int numOfBoats);
   ~Player();
   int getNumOfBoats();
+  void setGuess();
+  string getGuess();
+  *Board getBoard();
+  bool gettingShot(string location);
+  void shooting(string location, bool hit);
 
 private:
   int _numOfBoats;
+  string _guessShot;
+  Board *boardPtr;
 };
 
 #endif
