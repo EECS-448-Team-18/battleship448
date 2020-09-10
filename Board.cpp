c9d9a8aed0e155aed453245be58076b32b271694
Linh <linh.ng2000@gmail.com>
2020-09-09 21:27:31 -0500

    Upload Board Class

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,36 @@
+//Board.cpp
+#include "Board.h"
 
+Board::Board()
+{
+
+}
+
+Board::~Board()
+{
+
+}
+
+bool Board::shipLocation(int x, char y, char target)
+{
+        cout << "Enter a row number (between 1 to 9) and a column letter (between A to I): " << endl;
+        cin >> x;
+        cin >> y;
+        target = boardArray[x][y];
+}
+
+
+void Board::printBoard()
+{
+        cout << " A B C D E F G H I" <<endl;
+//      if(x >= 1 && x <= 9)
+        {
+                for(int i = 1; i < 9; i++)
+                {
+                        for(int j = 1; j < 9; j++)
+                        {
+                                cout << boardArray[i][j] << "\n";
+                        }
+                }
+        }
+}
--- Board.h
+++ Board.h
@@ -1,1 +1,25 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include "Ship.h"
+#include <iostream>
+using namespace std;
+
+class Board
+{
+        private:
+        //      int x; //rows
+        //      char y; //columns
+                char** boardArray;
+
+        public:
+                Board();
+                ~Board();
+                bool shipLocation(int x, char y);
+                void fire(int x, char y);
+        //      bool match();
+                void printBoard();
+
+
+};
+#endif
 
