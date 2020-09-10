3791100fc9844061f976f3a2842e33a7414716b4
Evelyn Thomas <evelynthomas@ku.edu>
2020-09-10 13:31:53 -0500

    printMyBoard function implemented

--- Board.cpp
+++ Board.cpp
@@ -11,20 +11,11 @@ Board::~Board()
 
 }
 
-bool Board::shipLocation(int x, char y, char target)
+void Board::printMyBoard()
 {
-        cout << "Enter a row number (between 1 to 9) and a column letter (between A to I): " << endl;
-        cin >> x;
-        cin >> y;
-        target = boardArray[x][y];
-}
-
-
-void Board::printBoard()
-{
-        cout << " A B C D E F G H I" <<endl;
+       // cout << " A B C D E F G H I" <<endl;
 //      if(x >= 1 && x <= 9)
-        {
+//        {
                 for(int i = 1; i < 9; i++)
                 {
                         for(int j = 1; j < 9; j++)
@@ -32,5 +23,17 @@ void Board::printBoard()
                                 cout << boardArray[i][j] << "\n";
                         }
                 }
-        }
+		cout << "\n";
+  //      }
 }
+
+void Board::printShotBoard()
+{
+
+}
+
+void Board::setNumberofShips(int shipNum)
+{
+	
+}
+
