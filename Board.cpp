d2638e2bfac26928aa72dca31a64b82aaa9bfaa7
x834l360 <x834l360@ku.edu>
2020-09-11 17:04:27 -0500

    Add setupBoard function to Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -59,4 +59,15 @@ void Board::getNumberofShips() const
 {
 	return(numberOfShips);
 }
+void Board::setupBoard()
+{
+        for(int i = 1; i < 9; i++)
+                {
+                        for(int j = 1; j < 9; j++)
+                        {
+                                myBoard[i][j]="blueTilde";
+                        }
+                }
+        
+}
 
