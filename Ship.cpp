aba3feb6ac6271abb828de69210c4e397d584bff
absaxen <70826971+absaxen@users.noreply.github.com>
2020-09-13 01:37:50 -0500

    Update Ship.cpp
    UPDATES: Fixed spelling errors/syntax errors. Fixed isSunk() function is a bool but was returning length.

--- Ship.cpp
+++ Ship.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
-Ship::Ship(){}
-
-Ship::~Ship(){
-    delete m_ship;
+Ship::Ship()
+{
+  m_ship = nullptr;   
+  m_length = 0;      
+  m_damage = 0;       
 }
 
-void Ship::createShip(int length) {
+Ship::~Ship(){
+    delete[] m_ship;
+}
+
+void Ship::createShip(int length) 
+{
+    m_length = length;
     m_ship = new string[length];
 }
 
@@ -21,25 +28,33 @@ string Ship::getCoordinate(int index) const{
 }
 
 void Ship::setLength(int length){
-    this->length = length;
+    m_length = length;
 }
 
 int Ship::getLength() const{
-    return(length);
+    return(m_length);
 }
 
-bool Ship::isSunk(){
-    return (length == 0);
+bool Ship::isSunk()
+{
+     if(m_damage == m_length)    //damage equals the length
+  {
+    return true;    //the ship is sunk
+  }
+  else    //damage does not equal the length
+  {
+    return false;   //the ship is not sunk
+  }
 }
 
 void Ship::addDamage(){
-    damage++;
+    m_damage++;
 }
 
 int Ship::getDamage() const{
-    return damage;
+    return m_damage;
 }
 
 void Ship::setDamage(int newDamage){
-    damage = newDamage;
+    m_damage = newDamage;
 }
