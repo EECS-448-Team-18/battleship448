1f2e3a06491f5b27b1bb3226550c44cf5621741e
absaxen <70826971+absaxen@users.noreply.github.com>
2020-09-13 21:04:36 -0500

    Updated Documentation for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-
+/*-----------------------------------------------------------------------------------
+WELCOME TO THE BATTLESHIP GAME FOR 2 PLAYERS!
+MADE BY: EECS 448 (FALL 2020) TEAM 18
+TEAM MEMBERS: Abhigyan Saxena, Achyut Paudel, Xiaoyi Lu, Linh Nguyen, Evelyn Thomas
+Filename: main.cpp 
+Author: Abhigyan Saxena
+Date: 9/06/2020
+Desc: main.cpp file sending functionality to a executive file.
+------------------------------------------------------------------------------------*/
 #include <iostream>
-#include "Executive.h"			//game tasks
+#include "Executive.h"		
 
 int main()
 {
 	std::cout << "Welcome to BattleShip! By EECS448-Team-18\n";		
-	Executive exec;	//executive object for all game tasks
+	Executive exec;	
 	return 0;	
 }
